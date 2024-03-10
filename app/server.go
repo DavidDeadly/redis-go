5becb4fbb5fd70d6c40182dfeb74917aa3a21d67
DavidDeadly <jdrueda513@gmail.com>
2024-03-10 00:33:24 -0500

    create exitOn function to handle errors

--- app/server.go
+++ app/server.go
@@ -10,14 +10,15 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
   listener, err := net.Listen("tcp", "0.0.0.0:6379")
-  if err != nil {
-    fmt.Println("Failed to bind to port 6379")
-    os.Exit(1)
-  }
+  exitOn(err, "Failed to bind to port 6379")
 
   _,  err = listener.Accept()
+  exitOn(err, "Error accepting connection")
+}
+
+func exitOn(err error, msg string) {
   if err != nil {
-    fmt.Println("Error accepting connection: ", err.Error())
+    fmt.Printf("%s: %v", msg, err.Error())
     os.Exit(1)
   }
 }
