3fc2098a306086c2dcce65489b5d0c89c8c846ea
DavidDeadly <jdrueda513@gmail.com>
2024-03-11 20:53:38 -0500

    format & add expiration feature to set command

--- app/server.go
+++ app/server.go
@@ -8,49 +8,67 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"time"
 )
 
+var Expiry = make(chan string)
+
 const (
-  OK = "OK"
-  PONG = "PONG"
-  NOT_FOUND = "$-1\r\n"
+	OK        = "OK"
+	PONG      = "PONG"
+	NOT_FOUND = "$-1\r\n"
 )
 
 var Data = map[string]string{}
 
-type CommandHandler func (params []string) []byte
+type CommandHandler func(params []string) []byte
+
 var commandHandlers = map[string]CommandHandler{
-
-  "PING": func(_ []string) []byte {
+	"PING": func(_ []string) []byte {
 		return SimpleString(PONG)
-  },
+	},
 
-  "ECHO": func(params []string) []byte {
-    message := strings.Join(params, " ")
+	"ECHO": func(params []string) []byte {
+		message := strings.Join(params, " ")
 
-    return SimpleString(message)
-  },
+		return SimpleString(message)
+	},
 
-  "SET": func(params []string) []byte {
-    key, value := params[0], params[1]
+	"SET": func(receivedParams []string) []byte {
+		params := make([]string, 4)
+		copy(params, receivedParams)
 
-    Data[key] = value
+		key, value := params[0], params[1]
 
-    return SimpleString(OK)
-  },
+		px := strings.ToUpper(params[2])
+		expireTime, _ := strconv.Atoi(params[3])
 
-  "GET": func(params []string) []byte {
-    key := params[0]
+		if expireTime != 0 {
+			fmt.Println("Px: ", px, " Time: ", expireTime)
+			fmt.Println(params)
+      
+      go Expire(key, expireTime, Expiry)
 
-    value, ok := Data[key]
+      fmt.Println("\nBLOCKED??")
+		}
 
-    if !ok {
-      var msg *string
-      return BulkString(msg)
-    }
+		Data[key] = value
 
-    return SimpleString(value)
-  },
+		return SimpleString(OK)
+	},
+
+	"GET": func(params []string) []byte {
+		key := params[0]
+
+		value, ok := Data[key]
+
+		if !ok {
+			var msg *string
+			return BulkString(msg)
+		}
+
+		return SimpleString(value)
+	},
 }
 
 func main() {
@@ -64,6 +82,8 @@ func main() {
 
 	defer listener.Close()
 
+  go ListenExpirations()
+
 	for {
 		var connection net.Conn
 		connection, err = listener.Accept()
@@ -75,6 +95,19 @@ func main() {
 		fmt.Println("\nNew connection")
 		go handleConnection(connection)
 	}
+}
+
+func ListenExpirations() {
+  for key := range Expiry {
+    fmt.Println("Deleted")
+    delete(Data, key)
+  }
+}
+
+func Expire(key string, timeMs int, channel chan string) {
+  time.Sleep(time.Millisecond * time.Duration(timeMs))
+  fmt.Printf("%d Milliseconds has passed %s expired...", timeMs, key)
+  channel <- key
 }
 
 func handleConnection(conn net.Conn) {
@@ -92,13 +125,12 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 
-    command, params := parseRedisProtocolRequest(string(request[:reqBytes]))
+		command, params := parseRedisProtocolRequest(string(request[:reqBytes]))
 
-    fmt.Printf("Command: '%s', Params: '%s'\n", command, params)
+		fmt.Printf("Command: '%s', Params: '%s'\n", command, params)
 
 		response := commandHandlers[command](params)
-    bytes, err := conn.Write(response)
-
+		bytes, err := conn.Write(response)
 		if err != nil {
 			printError(err, "Error sending data to the connection")
 			return
@@ -109,29 +141,32 @@ func handleConnection(conn net.Conn) {
 }
 
 func SimpleString(message string) []byte {
-  return []byte(fmt.Sprintf("+%s\r\n", message))
+	return []byte(fmt.Sprintf("+%s\r\n", message))
+}
+
+func SimpleError(err error) []byte {
+	return []byte(fmt.Sprintf("-%s\r\n", err.Error()))
 }
 
 func BulkString(message *string) []byte {
-  if message == nil {
-    return []byte(NOT_FOUND)
-  }
+	if message == nil {
+		return []byte(NOT_FOUND)
+	}
 
-  return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(*message), *message))
+	return []byte(fmt.Sprintf("$%d\r\n%s\r\n", len(*message), *message))
 }
 
 func parseRedisProtocolRequest(request string) (string, []string) {
-  places := strings.Split(request, "\r\n")
-  places = places[:len(places) - 1]
+	places := strings.Split(request, "\r\n")
+	places = places[:len(places)-1]
 
 	regex := regexp.MustCompile(`[\$\*]\d+`)
-  numElements, err := strconv.Atoi(places[0][1:])
+	numElements, err := strconv.Atoi(places[0][1:])
+	if err != nil {
+		fmt.Println("error parsing redis-cli message")
 
-  if err != nil {
-    fmt.Println("error parsing redis-cli message")
-
-    return "DEFAULT", []string{}
-  }
+		return "DEFAULT", []string{}
+	}
 
 	parsedMessage := make([]string, 0, numElements)
 
@@ -139,18 +174,18 @@ func parseRedisProtocolRequest(request string) (string, []string) {
 		matches := regex.MatchString(data)
 
 		if !matches {
-      parsedMessage = append(parsedMessage, data)
+			parsedMessage = append(parsedMessage, data)
 		}
 	}
 
-  if len(parsedMessage) == 0 {
-    fmt.Println("error getting the command and params from the request, none of them")
-    return "DEFAULT", []string{}
-  }
+	if len(parsedMessage) == 0 {
+		fmt.Println("error getting the command and params from the request, none of them")
+		return "DEFAULT", []string{}
+	}
 
-  command := strings.ToUpper(parsedMessage[0])
+	command := strings.ToUpper(parsedMessage[0])
 
-  return command, parsedMessage[1:]
+	return command, parsedMessage[1:]
 }
 
 func printError(err error, msg string) {
